cbd50928de455b54af47f266afa9d7fb53603247
Dynnho <dynnho23@hotmail.com>
2026-01-08 13:54:25 -0300

    feat: add routes for memory management including create, get all, and get by ID

--- PROJETO_MEMORIES/backend/app.js
+++ PROJETO_MEMORIES/backend/app.js
@@ -6,7 +6,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+require("./db/conn");
+
 const port = 3000;
+
+const memoryRoutes = require("./routes");
+
+app.use("/memories", memoryRoutes);
 
 app.listen(port, async () => {
   console.log(`O servidor iniciou na porta ${port}`);
